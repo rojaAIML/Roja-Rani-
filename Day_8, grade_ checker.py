ea5d014c8a5979eb87d48072ad4c1a689ac98b66
rojaAIML <rojarani.ml68@gmail.com>
2025-12-12 22:20:19 +0530

    Create Day_8, grade_ checker.py

---
+++ Day_8, grade_ checker.py
@@ -0,0 +1,12 @@
+mark = int(input("Enter your mark: "))
+
+if mark >= 90:
+    print("Grade: A")
+elif mark >= 75:
+    print("Grade: B")
+elif mark >= 60:
+    print("Grade: C")
+elif mark >= 45:
+    print("Grade: D")
+else:
+    print("Grade: Fail")
